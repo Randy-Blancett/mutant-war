a9784d91d1810eba6b971a42d717f431e56fb559
Randy.Blancett <Randy.Blancett@gmail.com>
2014-10-12 19:48:10 -0400

    Started working on testing database functions

--- src/test/bootstrap.php
+++ src/test/bootstrap.php
@@ -1,6 +1,33 @@
 <?php
 define ( 'BASE_PATH', dirname ( dirname ( __FILE__ ) ) . "/main/php/" );
 define ( 'TEST_PATH', dirname ( __FILE__ ) );
+define ( 'CON_NAME', "mutant_war" );
 
 // Include path
 set_include_path ( '.' . PATH_SEPARATOR . BASE_PATH . PATH_SEPARATOR . get_include_path () );
+
+// setup the autoloading
+require_once 'Propel/autoload.php';
+use Propel\Runtime\Propel;
+use Propel\Runtime\Connection\ConnectionManagerSingle;
+$serviceContainer = Propel::getServiceContainer ();
+$serviceContainer->setAdapterClass ( CON_NAME, 'sqlite' );
+$manager = new ConnectionManagerSingle ();
+$manager->setConfiguration ( array (
+        'dsn' => 'sqlite::memory:' ) );
+$serviceContainer->setConnectionManager ( CON_NAME, $manager );
+
+$objCon = Propel::getConnection ( CON_NAME );
+
+print_r ( $objCon );
+
+// $objCon->exec ( "show" );
+$tablesquery = $objCon->exec ( "CREATE DATABASE `test`;" );
+$tablesquery = $objCon->query ( "SELECT name FROM sqlite_master WHERE type='table';" );
+
+while ( $table = $tablesquery->fetch ( SQLITE3_ASSOC ) )
+{
+    echo $table ['name'] . '<br />';
+}
+// $objCon->;
+
