98f9f9784a6c2993982f0744534d3e8884189b26
Randy.Blancett <Randy.Blancett@gmail.com>
2014-11-05 18:48:29 -0500

    Got Htaccess working so that /page/ will send you into the phar file

--- www/index.php
+++ www/index.php
@@ -1,2 +1,10 @@
 <?php
-require "MutantWar.phar";
+require_once "phar://MutantWar.phar/MP_Autoloader.php";
+
+$file = trim ( substr ( $_SERVER [REQUEST_URI], strpos ( $_SERVER [REQUEST_URI], "/", 1 ) ), "/" );
+
+require_once "phar://MutantWar.phar/" . $file . ".php";
+
+print $file;
+
+//require "MutantWar.phar";
