4ccc9e62f20b595140880f584a21d3cdeead4dd2
Randy.Blancett <Randy.Blancett@gmail.com>
2014-11-08 16:23:23 -0500

    put boot strap in and got index displaying some basic layouts

--- src/main/www/index.php
+++ src/main/www/index.php
@@ -1,3 +1,30 @@
-this is the Index.php
-
-<?php
+<html>
+<head>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="css/bootstrap.min">
+</head>
+<body>
+	<div class="container-fluid">
+		<h1>Mutant War!!!</h1>
+		<div class="row">
+			<div class="col-sm-3" style="background-color: lavender;">
+				<form role="form">
+					<div class="form-group">
+						<label for="email">User Name:</label> <input type="text"
+							class="form-control" id="user_name" placeholder="User Name">
+					</div>
+					<div class="form-group">
+						<label for="pwd">Password:</label> <input type="password"
+							class="form-control" id="pwd" placeholder="Enter password">
+					</div>
+					
+					<button type="submit" class="btn btn-default">Submit</button>
+				</form>
+			</div>
+			<div class="col-sm-9" style="background-color: lavenderblush;">
+				<p>Two Thing</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
--- www/index.php
+++ www/index.php
@@ -1,10 +1,31 @@
 <?php
 require_once "phar://MutantWar.phar/MP_Autoloader.php";
 
-$file = trim ( substr ( $_SERVER [REQUEST_URI], strpos ( $_SERVER [REQUEST_URI], "/", 1 ) ), "/" );
+$strBaseRequest = trim ( substr ( $_SERVER ["REQUEST_URI"], strpos ( $_SERVER ["REQUEST_URI"], "/", 1 ) ), "/" );
+$intEndOfType = strpos ( $strBaseRequest, "/", 1 );
+$strType = substr ( $strBaseRequest, 0, $intEndOfType );
+$strFile = substr ( $strBaseRequest, $intEndOfType );
+$strFile = trim ( $strFile, "/" );
 
-require_once "phar://MutantWar.phar/" . $file . ".php";
+$strExt = "";
+switch ($strType)
+{
+    case "js" :
+        header ( 'Content-Type: application/javascript' );
+        $strFile = "js/" . $strFile;
+        $strExt = ".js";
+        break;
+    case "css" :
+        header ( 'Content-Type: text/css' );
+        $strFile = "css/" . $strFile;
+        $strExt = ".css";
+        break;
+    case "php" :
+        $strExt = ".php";
+        break;
+    default :
+        $strFile = $strType . "/" . $strFile;
+        $strExt = ".php";
+}
 
-print $file;
-
-//require "MutantWar.phar";
+require_once "phar://MutantWar.phar/" . $strFile . $strExt;
